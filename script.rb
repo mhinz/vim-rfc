fbfb3ea109cbf2b5b32ce9d53a2c11a2c3dfc252
Marco Hinz <mhinz@spline.de>
2012-10-29 09:46:15 +0100

    event-driven parsing + cachefile

--- script.rb
+++ script.rb
@@ -1,35 +1,52 @@
 #!/usr/bin/env ruby
 
 require 'nokogiri'
+require 'yaml'
 require 'net/http'
 
 module VimRFC
-  class MyDoc < Nokogiri::XML::SAX::Document
+
+  $rfchash   = {}
+  $rfcfile   = 'test.xml'
+  $cachefile = 'test.yml'
+
+  class Parser < Nokogiri::XML::SAX::Document
     def start_element(elem, attrs = [])
-      if elem == 'doc-id' || elem == 'title'
-        @readmode = true
+      case elem
+      when 'rfc-entry' then @in_rfc   = true
+      when 'doc-id'    then @in_docid = true
+      when 'title'     then @in_title = true
       end
-      #p Hash[*attrs.flatten]
     end
 
     def end_element(elem)
-      @readmode = false
+      case elem
+      when 'rfc-entry' then @in_rfc   = false and @docid = nil and @title = nil
+      when 'doc-id'    then @in_docid = false
+      when 'title'     then @in_title = false
+      end
     end
 
     def characters(s)
-      puts s if @readmode
+      if @in_rfc
+        if @in_docid
+          @docid = s
+        elsif @in_title
+          @title = s
+        end
+        $rfchash[@title.to_sym] = @docid if @docid and @title
+      end
     end
   end
 
-  class RFCHandling
+  class Handling
     def initialize
-      @rfc = '/data/rfc/test.xml'
-      update unless File.exist? @rfc
+      update unless File.exist? $rfcfile
     end
 
     def update
       Net::HTTP.start('www.ietf.org') do |http|
-        f = open(@rfc, 'w')
+        f = open($rfcfile, 'w')
         begin
           http.request_get('/rfc/new-rfc-index.xml') do |res|
             res.read_body do |segment|
@@ -47,7 +64,7 @@ module VimRFC
 
     def search
       counter = 0
-      f = File.new(@rfc)
+      f = File.new($rfcfile)
       begin
         while (line = f.gets)
           puts "#{counter}: #{line}"
@@ -61,10 +78,24 @@ module VimRFC
       end
     end
 
+    def load_from_file
+      require 'yaml'
+      $rfchash = YAML::load_file($cachefile)
+      puts $rfchash.inspect
+    end
+
+    def save_to_file
+      File.open($cachefile, 'w') do |f|
+        f.write $rfchash.to_yaml
+      end
+    end
+
     def parse
-      Nokogiri::XML::SAX::Parser.new(VimRFC::MyDoc.new).parse_file(@rfc)
+      Nokogiri::XML::SAX::Parser.new(VimRFC::Parser.new).parse_file($rfcfile)
     end
   end
 end
 
-VimRFC::RFCHandling.new.parse
+foo = VimRFC::Handling.new
+foo.parse
+foo.save_to_file
